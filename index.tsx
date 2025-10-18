0f16edad56b5247c643b06d00bfa24c8be9a1868
yotei98 <jelenasoon@gmail.com>
2025-10-18 20:01:52 +0800

    Simplify Tailwind CSS setup by removing PostCSS configuration

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './src/index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
