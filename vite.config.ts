5d9f879a636b90204aef983424385eb5fb21a8c4
yotei98 <jelenasoon@gmail.com>
2025-10-18 19:59:36 +0800

    Fix PostCSS configuration for Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
